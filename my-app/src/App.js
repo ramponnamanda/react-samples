b9e6ba61a409e2bf0decd83499e7a348ebdeaa05
Sitaramaswamy Ponnamanda <psrswami92@gmail.com>
2023-02-21 15:29:15 +0000

    using an array and map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,8 +9,6 @@ function Board({isXNext, squares, onPlay}) {
   const winner = calculateWinner(squares);
   let status = winner ? "Winner : " + winner : "Next Player : " + (isXNext ? "X" : "O");
 
-
-
   function handleClick(i) {
     if(squares[i] || winner)
       return;
@@ -20,24 +18,26 @@ function Board({isXNext, squares, onPlay}) {
     onPlay(nextSquares);
   }
 
+
+  const boardArray = [[0,1,2],[3,4,5],[6,7,8]];
+
+  const boardComponent = boardArray.map(row => {
+    return <div className="board-row">
+      {
+        row.map(i => {
+          return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+        })
+      }
+    </div>
+  });
+
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {
+        boardComponent
+      }
     </>
   );
 }
@@ -67,7 +67,7 @@ export default function Game() {
     } else {
       description = 'Go to game start';
     }
-    
+
     if(move === currentMove) {
       return <li key={move}>{description}</li>
     }
