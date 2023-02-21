68e0df94ca0229b62d784ebae68de42be0ef9c55
Sitaramaswamy Ponnamanda <psrswami92@gmail.com>
2023-02-21 15:09:29 +0000

    current move text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -61,11 +61,17 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
+
     if (move > 0) {
       description = 'Go to move #' + move;
     } else {
       description = 'Go to game start';
     }
+    
+    if(move === currentMove) {
+      return <li key={move}>{description}</li>
+    }
+
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
